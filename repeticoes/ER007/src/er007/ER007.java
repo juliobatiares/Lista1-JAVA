19c144ffb7f6424263951ce35d999d05a4732750
Julio Batiares <juliobatiares@gmail.com>
2017-04-24 07:10:11 -0300

    ER007
    1st commit

---
+++ repeticoes/ER007/src/er007/ER007.java
@@ -0,0 +1,92 @@
+/*
+USANDO A ESTRUTURA DE REPETIÇÃO "WHILE"
+
+CALCULADORA
+1. SOMAR
+2. SUBTRAIR
+3. MULTIPLICAR
+4. DIVIDIR
+5. VERIFICA SE O NÚMERO É PAR OU ÍMPAR
+
+* LER 2 NÚMEROS
+* FAZER O CÁLCULO
+* MOSTRAR O RESULTADO
+* DESEJA REALIZAR OUTRA OPERAÇÃO?
+ */
+package er007;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Julio Batista
+ */
+public class ER007 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        float n1, n2, resultado, resultado2;
+        int i, tpOper, Continuar;
+        
+        for (i = 1; i < 2; i = Continuar) {
+            
+            System.out.println("Digite o primeiro número: ");
+            n1 = input.nextFloat();
+            System.out.println("Digite o segundo número: ");
+            n2 = input.nextFloat();
+            
+            System.out.println("Escolha o tipo de Operação: ");
+            System.out.println("(1) SOMA\n(2) SUBTRAÇÃO\n(3) MULTIPLICAÇÃO\n(4) DIVISÃO\n(5) PAR ou IMPAR");
+            tpOper = input.nextInt();
+            
+            if (tpOper == 1) {
+                resultado = n1 + n2;
+                System.out.println("A soma é: " + resultado);
+            } else {
+                if (tpOper == 2) {
+                    resultado = n1 - n2;
+                    System.out.println("A subtração é: " + resultado);
+                } else {
+                    if (tpOper == 3) {
+                        resultado = n1 * n2;
+                        System.out.println("A multiplicação é: " + resultado);
+                    } else {
+                        if (tpOper == 4) {
+                            resultado = n1 / n2;
+                            System.out.println("A divisão é: " + resultado);
+                        } else {
+                            if (tpOper == 5) {
+                                
+                                resultado = n1 % 2;
+                                resultado2 = n2 % 2;
+                                
+                                if (resultado == 0) {
+                                    System.out.println("O número " + n1 + " é par.");
+                                } else {
+                                    System.out.println("O número " + n1 + " é ímpar.");
+                                }
+                                if (resultado2 == 0) {
+                                    System.out.println("O número " + n2 + " é par.");
+                                } else {
+                                    System.out.println("O número " + n2 + " é ímpar.");
+                                }
+                            } else {
+                                System.out.println("Digite um valor Válido.");
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println();
+            System.out.println("Deseja Continuar?");
+            System.out.println("(1) Sim\n(2) Não");
+            Continuar = input.nextInt();
+        }                
+    }
+    
+}
